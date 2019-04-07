dabb231dac26316ed040ddd7a2bbe1450f37cc06
Zoltan Biro <36306066+ZolBiro@users.noreply.github.com>
2019-04-07 11:01:35 +0200

    Add files via upload

---
+++ LeapYearCalculator/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean leapYear = LeapYearCalculator.isLeapYear(1924);
+        System.out.println(leapYear);
+
+        Boolean leapYear2 = LeapYearCalculator.isLeapYear(1800);
+        System.out.println(leapYear2);
+    }
+}
